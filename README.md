f4873f6274b90b26ad84c1bc1fb6da58e563fae1
vagrant <vagrant@rails-dev-box>
2013-11-07 02:20:24 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '98a9c4402962720279ec1fd37548fd764aa4a92cfba59fdd7d54752f55f9b6b706a05017875c828b3949e28d854b91a01d0786c17f2093b1e61d9ca56bbf57bd'
