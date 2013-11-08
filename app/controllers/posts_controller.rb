5a7f8c8503bcae3ad81c734a90d9fa18743cd2d4
Bryan Sattler <bsattler@gmail.com>
2013-11-07 23:01:38 -0500

    Added create and update CRUD actions and Posts nav link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,34 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    #raise
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])    
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end  
+
 end
