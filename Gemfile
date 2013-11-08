b3374dca4b032051ed4e179fbb305f78fe8bc129
Bryan Sattler <bsattler@gmail.com>
2013-11-07 22:03:10 -0500

    Installed Faker and seeded the database

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ end
 
 gem 'jquery-rails'
 
+# generate development data automatically
+gem 'faker'
+
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+rand(10..30).times do
+  p = Post.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+  rand(3..10).times do
+    p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  end
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
