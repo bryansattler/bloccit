4b795dd81a93f0966b44ecb88edc7aec494908b5
Bryan Sattler <bsattler@gmail.com>
2013-11-09 16:18:45 -0500

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Bloccit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { :host => 'bryansattler-bloccit.herokuapp.com' } 
 end
