c4c638dc8037639c6251eb2fab85da3ef640a92f
Bryan Sattler <bsattler@gmail.com>
2013-11-24 15:07:21 -0500

    Testing user_id login issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :comments
   # attr_accessible :title, :body
   has_many :posts
-  has_many :comments
+  has_many :comments
 
   before_create :set_member
   mount_uploader :avatar, AvatarUploader  
--- db/migrate/20131123183611_add_comments_to_users.rb
+++ db/migrate/20131123183611_add_comments_to_users.rb
@@ -1,5 +1,5 @@
 class AddCommentsToUsers < ActiveRecord::Migration
   def change
-    add_column :comments, :user_id, :string
+    add_column :comments, :user_id, :integer
   end
 end
