8588fd646613fe1f10713310299b39793a731e46
Bryan Sattler <bsattler@gmail.com>
2013-12-04 17:00:02 -0500

    Created show / hide jQuery function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function() {
+  $(".js-show-hide").click(function() {
+    var selector = "." + $(this).attr('data-selector');
+    if ($(selector).is(":visible")) {
+      $(selector).slideUp();
+    }
+    else {
+      $(selector).slideDown();
+    }
+    return false;
+  });
+});
